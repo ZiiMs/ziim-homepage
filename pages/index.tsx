fe0568470fc64988cb7f3775ed03bc65dcaf44cb
Alex Tedesco <alextede8899@gmail.com>
2021-09-23 10:38:52 -0400

    Footer complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,13 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <p>Yo its ZiiM</p>
+      <p>
+        I’m a full-stack engineer and a designer. I am fully self tought,
+        everything I know I learned on my own. I usually work with ReactJS
+        /NextJS and Chakra UI for front end. I have work with other languages
+        such as Java , lua and a tiny bit of C# . Currently I only do this for a
+        hobby. Though if you want any work from me, feel free to contact me.
+      </p>
     </div>
   );
 };

4b400c92efae6d1e4589d76903ac9bee408942a4
Alex Tedesco <alextede8899@gmail.com>
2021-09-24 10:18:24 -0400

    Add discord & popover

--- src/components/hero/image.tsx
+++ src/components/hero/image.tsx
@@ -7,7 +7,7 @@ const HeroImage = () => {
   return (
     <AspectRatio flexShrink={0} ratio={1} w={56} h={56}>
       <Box>
-        <Image src={Alex} placeholder='blur' />
+        <Image src={Alex} placeholder='blur' alt='ZiiM' />
       </Box>
     </AspectRatio>
   );

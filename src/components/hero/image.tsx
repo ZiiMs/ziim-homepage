e4f340c86db310e8cbed0cd68447502628adf98b
Alex Tedesco <alextede8899@gmail.com>
2021-09-25 06:53:55 -0400

    Add favicon

--- src/components/hero/image.tsx
+++ src/components/hero/image.tsx
@@ -1,16 +1,13 @@
 import { AspectRatio, Box, Spinner } from '@chakra-ui/react';
 import React from 'react';
 import Image from '../image';
+import Alex from './AlexTedesco.png';
 
 const HeroImage = () => {
   return (
     <AspectRatio flexShrink={0} ratio={1} w={56} h={56}>
       <Box>
-        <Image
-          src={'/assets/images/AlexTedesco.png'}
-          alt='ZiiM'
-          layout='fill'
-        />
+        <Image src={Alex} alt='ZiiM' layout='fill' />
       </Box>
     </AspectRatio>
   );

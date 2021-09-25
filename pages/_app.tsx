bec6cf44d07420252582d5d17a6b4303a748b481
Alex Tedesco <alextede8899@gmail.com>
2021-09-25 06:52:52 -0400

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,15 @@ import theme from 'src/theme';
 import Layout from '@/components/layout';
 
 import '../src/theme/style.css';
+import React from 'react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <link rel='icon' type='image/png' href='/assets/images/favicon.ico' />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/components/hero/image.tsx
+++ src/components/hero/image.tsx
@@ -1,13 +1,16 @@
 import { AspectRatio, Box, Spinner } from '@chakra-ui/react';
 import React from 'react';
 import Image from '../image';
-import Alex from './avataaars.png';
 
 const HeroImage = () => {
   return (
     <AspectRatio flexShrink={0} ratio={1} w={56} h={56}>
       <Box>
-        <Image src={Alex} placeholder='blur' alt='ZiiM' />
+        <Image
+          src={'/assets/images/AlexTedesco.png'}
+          alt='ZiiM'
+          layout='fill'
+        />
       </Box>
     </AspectRatio>
   );
